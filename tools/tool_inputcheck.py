dc188f38ce8dc387f32fdbcf8aa57d8042372b3c
Gene R <gururise@gmail.com>
2023-03-22 19:15:36 -0700

    TOOL: update regex for img

--- tools/tool_inputcheck.py
+++ tools/tool_inputcheck.py
@@ -10,7 +10,7 @@ with open(dataset, "r") as f:
 noinput_pattern = re.compile(r"[\[\(\<]?no[ ]?input[\]\)\>\.]?", re.IGNORECASE)
 
 # regex for "![any string](http" to detect if internet data is being passed into input
-img_pattern = re.compile(r'!\[.*\]\(http|img src=')
+img_pattern = re.compile(r'!\[.*\]\(http|img[ ]?src=|image:')
 
 issue_cnt = 0
 # Loop through JSON data and output items that contain "input" elements matching the regex
@@ -23,9 +23,9 @@ print(f"Identified {issue_cnt} potential <noinput> issues.")
 
 issue_cnt = 0
 #Loop through JSON data and output items that contain "input" elements matching the regex
-# print("![alt text] problems:")
-# for item in json_data:
-#     if "input" in item and img_pattern.search(item["input"]):
-#         print(item)
-#         issue_cnt += 1
-# print(f"Identified {issue_cnt} potential ![alt text] issues.")
+print("![alt text] problems:")
+for item in json_data:
+    if "input" in item and img_pattern.search(item["input"]):
+        print(item)
+        issue_cnt += 1
+print(f"Identified {issue_cnt} potential ![alt text] issues.")
