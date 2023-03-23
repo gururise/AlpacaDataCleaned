73f3b0883db3af572d60887ce2b5587f54c9cf90
Gene R <gururise@gmail.com>
2023-03-22 18:59:37 -0700

    TOOL: improve <noinput> regex

--- tools/tool_inputcheck.py
+++ tools/tool_inputcheck.py
@@ -7,7 +7,7 @@ with open(dataset, "r") as f:
     json_data = json.load(f)
 
 # regex for "<noinput>", "No Input", "<No input>", "noinput", "<no input>"
-noinput_pattern = re.compile(r"<no\s*input>|\(no input\)", re.IGNORECASE)
+noinput_pattern = re.compile(r"[\[\(\<]?no[ ]?input[\]\)\>\.]?", re.IGNORECASE)
 
 # regex for "![any string](http" to detect if internet data is being passed into input
 img_pattern = re.compile(r'!\[.*\]\(http|img src=')
@@ -22,10 +22,10 @@ for item in json_data:
 print(f"Identified {issue_cnt} potential <noinput> issues.")
 
 issue_cnt = 0
-# Loop through JSON data and output items that contain "input" elements matching the regex
-print("![alt text] problems:")
-for item in json_data:
-    if "input" in item and img_pattern.search(item["input"]):
-        print(item)
-        issue_cnt += 1
-print(f"Identified {issue_cnt} potential ![alt text] issues.")
+#Loop through JSON data and output items that contain "input" elements matching the regex
+# print("![alt text] problems:")
+# for item in json_data:
+#     if "input" in item and img_pattern.search(item["input"]):
+#         print(item)
+#         issue_cnt += 1
+# print(f"Identified {issue_cnt} potential ![alt text] issues.")
